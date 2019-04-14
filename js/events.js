950db38d3bf6a7fc651f0261823108f6cdad6cf8
Francis Lewis <francis.lewis@me.com>
2019-04-14 16:30:03 +0000

    Automatically backed up by Learn

--- js/events.js
+++ js/events.js
@@ -11,7 +11,7 @@ function frameIt() {
 }
 
 function pressIt() {
-  $('input').on('keydown', (e) => key.which === 71 ? alert('g was pressed') : return)
+  $('input').on('keydown', (e) => if (key.which === 71) alert('g was pressed'))
 }
 
 $(document).ready(function() {
