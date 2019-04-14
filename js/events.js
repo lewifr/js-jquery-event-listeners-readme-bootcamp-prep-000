3248c5225f211517a70efe988233079e52626608
Francis Lewis <francis.lewis@me.com>
2019-04-14 16:22:55 +0000

    Automatically backed up by Learn

--- js/events.js
+++ js/events.js
@@ -3,12 +3,15 @@
 function getIt() {
   $('p').on('click', function() { alert('Hey!') })
 }
+
+function frameIt() {
+  $('img').on('load', function(e) {
+  //  e.target[class] = 'tasty'
+  })
+}
+
 $(document).ready(function() {
 
 
-  function frameIt() {
-    $('img').on('load', function(e) {
-    //  e.target[class] = 'tasty'
-    })
-  }
+
 });
